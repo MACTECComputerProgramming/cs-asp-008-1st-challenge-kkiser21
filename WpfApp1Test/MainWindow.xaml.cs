3d3a75b01b8044f62d389c573bcf2859a5b38418
Drewwheeler2 <70332740+Drewwheeler2@users.noreply.github.com>
2020-12-11 09:46:41 -0600

    commit

---
+++ WpfApp1Test/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Multiplication_Click(object sender, RoutedEventArgs e)
+        {
+            ans.Content = Convert.ToInt32(fnum.Text) * Convert.ToInt32(snum.Text);
+        }
+
+        private void Addition_Click(object sender, RoutedEventArgs e)
+        {
+            ans.Content = Convert.ToInt32(fnum.Text) + Convert.ToInt32(snum.Text);
+        }
+
+        private void Subtraction_Click(object sender, RoutedEventArgs e)
+        {
+            ans.Content = Convert.ToInt32(fnum.Text) - Convert.ToInt32(snum.Text);
+
+        }
+
+        private void Division_Click(object sender, RoutedEventArgs e)
+        {
+            ans.Content = Convert.ToInt32(fnum.Text) / Convert.ToInt32(snum.Text);
+
+        }
+
+        private void Modules_Click(object sender, RoutedEventArgs e)
+        {
+            ans.Content = Convert.ToInt32(fnum.Text) % Convert.ToInt32(snum.Text);
+
+        }
+    }
+}
